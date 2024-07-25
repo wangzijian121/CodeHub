151157d9c61631141185081ed994ec516bef424d
zijian Wang <1219116968@qq.com>
2024-07-25 22:03:34 +0800

    update：更新目录结构

--- spring-boot/src/main/java/com/zjyun/springboot/springboot集成mp增删改查/config/StdoutLogger.java
+++
@@ -1,7 +0,0 @@
-package com.zjyun.springboot.springboot集成mp增删改查.config;
-
-public class StdoutLogger extends com.p6spy.engine.spy.appender.StdoutLogger {
-    public void logText(String text) {
-        System.err.println("SQL" + text);
-    }
-}
