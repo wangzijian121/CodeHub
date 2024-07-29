af1d9fd7eafecc8f8b7541eec2c0f4455208e784
王子健 <1219116968@qq.com>
2024-07-29 09:44:51 +0800

    feature: springboot-日志与日志级别分组

--- spring-boot-yunwei/src/main/java/com/zjyun/springboot/controller/AccountController.java
+++ spring-boot-yunwei/src/main/java/com/zjyun/springboot/controller/AccountController.java
@@ -2,11 +2,14 @@ package com.zjyun.springboot.controller;
 
 import com.zjyun.springboot.entity.Account;
 import com.zjyun.springboot.service.IAccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * <p>
@@ -20,11 +23,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccountController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
     @Autowired
     private IAccountService accountService;
 
     @GetMapping("/{id}")
     public Account getAccount(@PathVariable Integer id) {
         return accountService.getById(id);
+    }
+
+
+    @GetMapping("/log")
+    public void  getLog() {
+        logger.debug("wangzijian-debug");
+        logger.info("wangzijian-info");
+        logger.warn("wangzijian-warn");
+        logger.error("wangzijian-error");
     }
 }
