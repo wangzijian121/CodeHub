15238c210a4da4ebb39e5fa5b97b4d20690ae007
王子健 <1219116968@qq.com>
2024-08-04 15:45:48 +0800

    update：修改包前缀命名

--- springboot-yunwei/src/main/java/com/zjyun/springboot/controller/AccountController.java
+++ springboot-yunwei/src/main/java/com/zjyun/springboot/controller/AccountController.java
@@ -3,8 +3,6 @@ package com.zjyun.springboot.controller;
 import com.zjyun.springboot.entity.Account;
 import com.zjyun.springboot.service.IAccountService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
