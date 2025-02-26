8f350bb7804b9b0bd9fba41d8cb4aa8e82612c0c
王子健 <1219116968@qq.com>
2025-02-26 11:33:46 +0800

    feature：spark-rdd 排名问题

---
+++ spark/src/main/scala/com/zjyun/spark/rdd/转换类算子/访问量top测试/TestTop.scala
@@ -0,0 +1,56 @@
+package com.zjyun.spark.rdd.转换类算子.访问量top测试
+
+import com.zjyun.spark.utils.Utils.initSc
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object TestTop {
+
+  //首先求出【整个文档】中【老师访问量】最高的top3
+
+  /**
+   * (laoyang,53)
+   * (xiaohe,30)
+   * (laoliu,21)
+   * (laochen,15)
+   * (unclewang,7)
+   * (laozhang,6)
+   */
+  def top3 = {
+    val sc = initSc("top3")
+    val value = sc.textFile("src/main/scala/com/zjyun/spark/rdd/datas/urls.txt")
+    //(xiaohe,7) xiaohe 作为key
+    val res = value
+      .map(_.split(",")(1))
+      .map((_, 1))
+      .reduceByKey(_ + _)
+      .sortBy(-_._2)
+      .take(3)
+    res.foreach(x => println(x + " "))
+  }
+
+  //每个【专业】中【访问量】最高的top2
+  def top2 = {
+
+    val sc = initSc("top2")
+    val value = sc.textFile("src/main/scala/com/zjyun/spark/rdd/datas/urls.txt")
+    val res = value
+      .map(line => {
+        val strings = line.split(",")
+        ((strings(0), strings(1)), 1)
+      })
+      .reduceByKey(_ + _)                               //((java,laochen),15)
+      .map(item => (item._1._1, (item._1._2, item._2)))//(java,(laochen,15))
+      .groupByKey()       //(java,CompactBuffer((laochen,15), (laoliu,21), (laozhang,6), (xiaohe,11)))
+      .mapValues(
+        item => item.toList.sortBy(-_._2)//按Value中的指定位数排序
+      )
+
+    res.collect().foreach(x => println(x + " "))
+  }
+
+  def main(args: Array[String]): Unit = {
+    top2
+    top3
+  }
+}
--- spark/src/main/scala/com/zjyun/spark/utils/Utils.scala
+++ spark/src/main/scala/com/zjyun/spark/utils/Utils.scala
@@ -1,7 +1,16 @@
 package com.zjyun.spark.utils
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 case object Utils {
   def printType[T](value: T): Unit = {
     println(s"内容: $value, 类型: ${value.getClass.getName}")
+  }
+
+  def initSc(name: String) = {
+    val conf = new SparkConf()
+    conf.setMaster("local[*]")
+    conf.setAppName(name)
+    new SparkContext(conf)
   }
 }
