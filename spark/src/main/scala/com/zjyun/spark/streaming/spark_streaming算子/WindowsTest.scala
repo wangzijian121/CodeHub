6977b58cdb5db1e4cc9fe4f5270fe31294d7ac29
王子健 <1219116968@qq.com>
2025-04-02 19:17:15 +0800

    feature:spark-streaming窗口函数

---
+++ spark/src/main/scala/com/zjyun/spark/streaming/spark_streaming算子/WindowsTest.scala
@@ -0,0 +1,30 @@
+package com.zjyun.spark.streaming.spark_streaming算子
+
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
+
+/**
+ * Windows 窗口函数
+ * 需求：每5秒计算前30秒银行交易额
+ * wangzijian:666 交易666 金额
+ */
+object WindowsTest {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setMaster("local[2]").setAppName("sparkStreaming")
+    val ssc = new StreamingContext(conf, Seconds(5))
+    val receiverInputDStream: ReceiverInputDStream[String] = ssc.socketTextStream("localhost", 6666)
+
+    receiverInputDStream.window(Seconds(30)).foreachRDD((rdd, time) => {
+      rdd.map(line => {
+          val lineSplit = line.split(":")
+          (lineSplit(0), lineSplit(1).toInt)
+        })
+        .reduceByKey(_ + _)
+        .foreach(println)
+    })
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
