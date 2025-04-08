aaaf251720be080f40401c11ede713899ed79495
王子健 <1219116968@qq.com>
2025-04-08 11:28:34 +0800

    feature:spark-streaming重命名文件

--- spark/src/main/scala/com/zjyun/spark/streaming/a_spark_streaming算子/UpdateStateByKeyCkptTest.scala
+++ spark/src/main/scala/com/zjyun/spark/streaming/a_spark_streaming算子/UpdateStateByKeyCkptTest.scala
@@ -7,29 +7,24 @@ import org.apache.spark.streaming.dstream.ReceiverInputDStream
 /**
  * 使用 checkpoint 来保存历史数据
  */
-object UpdateStateByKeyCkptTest {
+object UpdateStateByKeyCheckPointTest {
   def main(args: Array[String]): Unit = {
 
     val ckptPath = "./ckpt"
-    val conf = new SparkConf()
-      .setMaster("local[2]")
-      .setAppName("sparkStreaming")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("sparkStreaming")
     //如果ckptPath 路径有历史数据则加载，否则走自行创建StreamingContext的逻辑
     val ssc = StreamingContext.getActiveOrCreate(ckptPath, () => {
-
       val ssc = new StreamingContext(conf, Seconds(5))
       //设置checkpoint文件夹
       ssc.checkpoint("./ckpt")
       val receiverInputDStream: ReceiverInputDStream[String] = ssc.socketTextStream("localhost", 6666)
-
       receiverInputDStream.transform((rdd, time) => {
           rdd.flatMap(_.split(" "))
             .map((_, 1))
             .reduceByKey(_ + _)
         })
         //curr为同一个key对应的值列表，last为历史值
-        .updateStateByKey((curr: Seq[Int], last: Option[Int]) => Option(curr.sum + last.getOrElse(0))
-        ).print()
+        .updateStateByKey((curr: Seq[Int], last: Option[Int]) => Option(curr.sum + last.getOrElse(0))).print()
       ssc
     })
     ssc.start()
